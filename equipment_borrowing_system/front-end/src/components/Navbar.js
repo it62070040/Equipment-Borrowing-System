babc5b67a93a3094747c026a3b4fee31b4c46441
it62070040 <62070040@it.kmitl.ac.th>
2022-04-08 01:39:40 +0700

    feature: login, logout, profilePic, profileName

--- equipment_borrowing_system/front-end/src/components/Navbar.js
+++ equipment_borrowing_system/front-end/src/components/Navbar.js
@@ -3,8 +3,41 @@ import { Link } from "react-router-dom";
 import SignIn from "./GoogleSignIn";
 import "./Navbar.css";
 import logo from "../assets/EQ-logo.png"
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
+const clientId = "1089120979699-boinlps81kfjm5ptjhetjnbsj8cd1a2r.apps.googleusercontent.com";
+const settings = ['Logout'];
 function Navbar() {
+
+  const [showloginButton, setShowloginButton] = useState(true);
+    const [showlogoutButton, setShowlogoutButton] = useState(false);
+    const [showUsername, setShowUsername] = useState(false);
+    const [info, setInfo] = useState({});
+    const onLoginSuccess = (res) => {
+        console.log('Login Success:', res.profileObj);
+        setShowloginButton(false);
+        setShowUsername(true);
+        setInfo(res.profileObj)
+    };
+
+    const onLoginFailure = (res) => {
+        console.log('Login Failed:', res);
+    };
+
+    const onSignoutSuccess = () => {
+        alert("You have been logged out successfully");
+        console.clear();
+        setShowloginButton(true);
+        setShowlogoutButton(false);
+        setShowUsername(false);
+    };
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
 
@@ -17,6 +50,15 @@ function Navbar() {
       // console.log(button)
     }};
 
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+  
+    const handleOpenUserMenu = (event) => {
+      setAnchorElUser(event.currentTarget);
+    };
+  
+    const handleCloseUserMenu = () => {
+      setAnchorElUser(null);
+    };
 
   useEffect(() => {
     showButton();
@@ -68,17 +110,53 @@ function Navbar() {
             </li>
           </ul>
           <div className="btn-signin-contaier">
-            {button && (
-            <Link to="/sign-in" >
-              <button 
-                className="btn-signin"
-                // onClick={onClick}
-                // type={type}
-              >
-                Sign In
-              </button>
-            </Link>
-          )}
+          <button className="btn-signin">Sign In</button>
+          { showloginButton ?
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign In"
+                    onSuccess={onLoginSuccess}
+                    onFailure={onLoginFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                /> : null}
+            {showUsername ?
+               <Box sx={{ flexGrow: 0 }}>
+               <Tooltip title="Logout">
+                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                   <Avatar  src={info.imageUrl} />
+                   {info.givenName}
+                 </IconButton>
+               </Tooltip>
+               <Menu
+                 sx={{ mt: '45px' }}
+                 id="menu-appbar"
+                 anchorEl={anchorElUser}
+                 anchorOrigin={{
+                   vertical: 'top',
+                   horizontal: 'right',
+                 }}
+                 keepMounted
+                 transformOrigin={{
+                   vertical: 'top',
+                   horizontal: 'right',
+                 }}
+                 open={Boolean(anchorElUser)}
+                 onClose={handleCloseUserMenu}
+               >
+                 {settings.map((setting) => (
+                   <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                     <GoogleLogout
+                    clientId={clientId}
+                    buttonText="Sign Out"
+                    onLogoutSuccess={onSignoutSuccess}
+                >
+                </GoogleLogout>
+                   </MenuItem>
+                 ))}
+               </Menu>
+             </Box>
+             :null}
           </div>
           
         </div>
