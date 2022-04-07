05a3dc2f87aacec790398b52cfecafa0567cf24e
it62070040 <62070040@it.kmitl.ac.th>
2022-04-08 00:25:07 +0700

    feature: add backgroun, component, button

--- equipment_borrowing_system/front-end/src/pages/Home/Home.js
+++ equipment_borrowing_system/front-end/src/pages/Home/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { ReactDOM } from 'react-dom';
+import Button from '@mui/material/Button';
 import './Home.css'
 
 function Home() {
     return (
         <div className="home">
-            <div className="home-container">
-                <h1>Home</h1>
+            <div className="home-container" style={{backgroundColor: "white", margin: "100px", width: "35%", position: "relative", padding: "50px", fontSize: "2vw", borderRadius: "25px"}}>
+                <h3>ระบบยืม-คืนอุปกรณ์การศึกษา</h3>
+                <h1>Equipment</h1>
+                <h1>Borrowing</h1>
+                <h1>System</h1>
+                <Button variant="contained" size="large" style={{backgroundColor: "#6AB4FA", fontSize: "1.5vw"}}>Borrow Equipments</Button>
             </div>
         </div>
     )
