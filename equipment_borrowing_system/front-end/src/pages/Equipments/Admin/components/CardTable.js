cdc867b531881f531222e62d7c964b45923dab36
it62070041 <56312673+it62070041@users.noreply.github.com>
2022-04-08 23:20:24 +0700

    update: status in equipment page admin

--- equipment_borrowing_system/front-end/src/pages/Equipments/Admin/components/CardTable.js
+++ equipment_borrowing_system/front-end/src/pages/Equipments/Admin/components/CardTable.js
@@ -1,31 +1,30 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
 import * as React from "react";
 
-export default function CardTable({searchResult}) {
+export default function CardTable({ searchResult }) {
   return (
     <>
-     
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid container>
-            <Grid item xs={7}>
-              <Grid container>
-                <Grid item xs={3}></Grid>
-                <Grid item xs={9}>
-                  <p className="table-title">Title</p>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item xs={5}>
-              <Grid container>
-                <Grid item xs={3}>
-                  <p className="table-status">Status</p>
-                </Grid>
-                <Grid item xs={9}></Grid>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container>
+          <Grid item xs={8}>
+            <Grid container>
+              <Grid item xs={3}></Grid>
+              <Grid item xs={9}>
+                <p className="table-title">Title</p>
               </Grid>
             </Grid>
           </Grid>
-
-          {/* list equipment */} {searchResult?.map((item) => (
+          <Grid item xs={4}>
+            <Grid container>
+              <Grid item xs={3}>
+                <p className="table-status">Status</p>
+              </Grid>
+              <Grid item xs={9}></Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        {/* list equipment */}{" "}
+        {searchResult?.map((item) => (
           <Box
             sx={{
               flexGrow: 1,
@@ -35,22 +34,18 @@ export default function CardTable({searchResult}) {
               flexDirection: "column",
               display: "flex",
               justifyContent: "center",
-              my: 2
+              my: 2,
             }}
           >
             <Grid container>
-              <Grid item xs={7}>
+              <Grid item xs={8}>
                 <Grid container>
                   <Grid
                     item
                     xs={3}
                     sx={{ display: "flex", justifyContent: "center" }}
                   >
-                    <img
-                      className="table-img"
-                      alt="complex"
-                      src={item.url}
-                    />
+                    <img className="table-img" alt="complex" src={item.url} />
                   </Grid>
                   <Grid
                     item
@@ -62,15 +57,13 @@ export default function CardTable({searchResult}) {
                     }}
                   >
                     <p className="style-name">{item.name}</p>
-                    <p className="style-des">
-                    {item.description}
-                    </p>
+                    <p className="style-des">{item.description}</p>
                   </Grid>
                 </Grid>
               </Grid>
               <Grid
                 item
-                xs={5}
+                xs={4}
                 sx={{
                   flexDirection: "column",
                   display: "flex",
@@ -78,19 +71,29 @@ export default function CardTable({searchResult}) {
                 }}
               >
                 <Grid container>
-                  <Grid item xs={3}>
-                    {item.status === 'availble' ? <div style={{width: "10px", height: "10px", backgroundColor: "#008000"}}></div> : <div style={{width: "10px", height: "10px", backgroundColor: "#FF0000"}}></div>}
-                    <p className="style-status">{item.status}</p>
+                  <Grid item xs={4}>
+                    {item.status === "availble" ? (
+                      <p className="style-status" style={{ color: "#008000" }}>
+                        {item.status}
+                      </p>
+                    ) : (
+                      <p className="style-status" style={{ color: "#FF0000" }}>
+                        {item.status}
+                      </p>
+                    )}
                   </Grid>
-                  <Grid item xs={9}>
+                  <Grid item xs={8} sx={{ backgroundColor: "pink" }}>
                     <p className="style-btn">test</p>
+                    <Button variant="contained" sx={{backgroundColor: "#FF0000", textTransform: 'lowercase'}}>
+                      delete
+                    </Button>
                   </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Box>))}
-        </Box>
-      
+          </Box>
+        ))}
+      </Box>
     </>
   );
 }
