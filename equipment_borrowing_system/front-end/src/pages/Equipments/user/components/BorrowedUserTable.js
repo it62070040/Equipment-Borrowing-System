3862943d2cebea99f5d364dfb4d436728de75224
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2022-04-14 13:13:54 +0700

    fix: equipment table, add nevbar box-shadow

--- equipment_borrowing_system/front-end/src/pages/Equipments/user/components/BorrowedUserTable.js
+++ equipment_borrowing_system/front-end/src/pages/Equipments/user/components/BorrowedUserTable.js
@@ -9,29 +9,29 @@ export default function BorrowedUserTable({ user }) {
   //   );
 
   return (
-    <div className="table-container">
-      <div className="table-header-row-con">
-        <ul className="table-header-row">
-          <li className="table-header">No.</li>
-          <li className="table-header">Name</li>
-          <li className="table-header">Borrow Date</li>
-          <li className="table-header">Return Date</li>
-          <li className="table-header">Return Status</li>
+    <div className="borrow-table-container">
+      <div className="borrow-table-header-row-con">
+        <ul className="borrow-table-header-row">
+          <li className="borrow-table-header">No.</li>
+          <li className="borrow-table-header">Name</li>
+          <li className="borrow-table-header">Borrow Date</li>
+          <li className="borrow-table-header">Return Date</li>
+          <li className="borrow-table-header">Return Status</li>
         </ul>
       </div>
       {user.map((row, i) => (
         <div
-          className="user-row-con"
+          className="borrow-row-con"
           key={row.id}
           //   onClick={() => clickEqCard(row.id)}
         >
           <div className="user-row">
             <ul className="table-user-row">
-              <li className="table-item">{i + 1}</li>
-              <li className="table-item">{row.firstName} {row.lastName}</li>
-              <li className="table-item">{row.borrowDate}</li>
-              <li className="table-item">{row.returnDate}</li>
-              <li className="table-item" style={row.returnStatus === "Success" ? {color: "green"} : row.returnStatus === "Failed" ? {color: "red"} : {color: "gray"}}>{row.returnStatus}</li>
+              <li className="user-table-item">{i + 1}</li>
+              <li className="user-table-item">{row.firstName} {row.lastName}</li>
+              <li className="user-table-item">{row.borrowDate}</li>
+              <li className="user-table-item">{row.returnDate}</li>
+              <li className="user-table-item" style={row.returnStatus === "Success" ? {color: "green"} : row.returnStatus === "Failed" ? {color: "red"} : {color: "gray"}}>{row.returnStatus}</li>
             </ul>
           </div>
         </div>
