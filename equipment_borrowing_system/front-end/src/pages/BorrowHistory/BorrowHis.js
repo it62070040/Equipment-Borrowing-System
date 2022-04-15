bd7dc6c5057c6bbe8986d5bb1aa8c937074166b0
Pando5 <80274964+Pando5@users.noreply.github.com>
2022-04-15 22:25:33 +0700

    update : BorrowHistory

--- equipment_borrowing_system/front-end/src/pages/BorrowHistory/BorrowHis.js
+++ equipment_borrowing_system/front-end/src/pages/BorrowHistory/BorrowHis.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import './BorrowHis.css'
+import React from "react";
+import "./BorrowHis.css";
+import { Box } from "@mui/material";
+import MiniBarUser from "./User/MiniBar";
+import MiniBarAdmin from "./Admin/MiniBar";
 
 function BorrowHis() {
-    return (
-        <div className="borrowHis">
-            <div className="borrowHis-container">
-                <h1>Borrow History</h1>
-            </div>
-        </div>
-    )
+  return (
+    <div className="borrow">
+      <div className="borrow-container">
+        <h1 className="Header-title-Bor">History</h1>
+        <Box
+          className="search-bar"
+          sx={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            px: 5,
+          }}
+        >
+          {/* <MiniBarUser /> */}
+          <MiniBarAdmin />
+        </Box>
+        {/* <Box className="table" sx={{p: 1, mx:5, mt:4, backgroundColor: '#fff', borderRadius: '10px'}}>
+          <CardTable />
+        </Box> */}
+      </div>
+    </div>
+  );
 }
 
 export default BorrowHis;
