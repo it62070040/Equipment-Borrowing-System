9475fe1990f7e8009d3e1d3e056689e36836f5ae
it62070040 <62070040@it.kmitl.ac.th>
2022-05-23 00:15:26 +0700

    update login only using @it.kmitl.ac.th

--- equipment_borrowing_system/front-end/src/index.js
+++ equipment_borrowing_system/front-end/src/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache()
+});
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
