7f985c09e02fb7db33861ea78d6c9464b9839394
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2022-05-23 22:35:03 +0700

    update: home page

--- equipment_borrowing_system/back-end/src/graphql/query/order.js
+++ equipment_borrowing_system/back-end/src/graphql/query/order.js
@@ -5,13 +5,14 @@ import { OrderModel, OrderTC } from '../../models/order'
 export const Orders = OrderTC.getResolver('findMany')
 export const OrderId = OrderTC.getResolver('findById')
 
-export const UserOrder = schemaComposer.createResolver({
-  name: 'UserOrder',
-  kind: 'query',
-  type: OrderTC.getType(),
-  resolve: async ({ context }) => {
-    const { userId: _id } = context
-    const userOrder = await OrderModel.findById(_id)
-    return userOrder
-  },
-})
+// Query user's orders
+// export const UserOrder = schemaComposer.createResolver({
+//   name: 'UserOrder',
+//   kind: 'query',
+//   type: OrderTC.getType(),
+//   resolve: async ({ context }) => {
+//     const { userId: _id } = context
+//     const userOrder = await OrderModel.findById(_id)
+//     return userOrder
+//   },
+// })
