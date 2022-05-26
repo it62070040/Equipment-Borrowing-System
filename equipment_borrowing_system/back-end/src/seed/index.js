153f3e98a21dd81238c88c6d0bb23dde72b43a49
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2022-05-27 01:37:41 +0700

    update: seed

--- equipment_borrowing_system/back-end/src/seed/index.js
+++ equipment_borrowing_system/back-end/src/seed/index.js
@@ -20,7 +20,7 @@ const EQUIPMENTS = [
     description: 'เมาส์ สีดำ MD Tech MD-17',
     category: 'Electronics',
     url_pic: 'https://aumento.officemate.co.th/media/catalog/product/O/F/OFM4005413.jpg?imwidth=640',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -28,7 +28,7 @@ const EQUIPMENTS = [
     description: 'Primaxx KB-509 คีย์บอร์ดมีสาย',
     category: 'Electronics',
     url_pic: 'https://cf.shopee.co.th/file/d94f1429b7a4404b93e578724c3b6bf5',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -36,7 +36,7 @@ const EQUIPMENTS = [
     description: 'Audio Technica หูฟัง รุ่น ATH-770XCOM Stereo Headset',
     category: 'Electronics',
     url_pic: 'https://f.btwcdn.com/store-30005/product/d0d8e521-fc57-0d15-cd52-602f858f4d2b.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -44,7 +44,7 @@ const EQUIPMENTS = [
     description: 'Boya BY-WFM12 VHF Wireless Microphone',
     category: 'Electronics',
     url_pic: 'https://www.digital2home.com/wp-content/uploads/2019/10/Boya-BY-WFM12-warranty-01.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -52,7 +52,7 @@ const EQUIPMENTS = [
     description: 'XPPen Star G430S เมาส์ปากกา ขนาด 4x3 นิ้ว แรงกด 8192 ระดับ',
     category: 'Electronics',
     url_pic: 'https://mercular.s3.ap-southeast-1.amazonaws.com/images/products/2022/01/Product/xp-pen-star-g430s-pen-mouse-01.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -60,7 +60,7 @@ const EQUIPMENTS = [
     description: 'LOGITECH M221 WIRELESS SILENT (BLACK)',
     category: 'Electronics',
     url_pic: 'https://www.jib.co.th/img_master/product/original/20180712130913_25451_11963954.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -68,7 +68,7 @@ const EQUIPMENTS = [
     description: 'Webcam FULL HD 1080p กล้องเว็บแคม ยี่ห้อ ASHU รุ่น H800',
     category: 'Electronics',
     url_pic: 'https://inwfile.com/s-fy/fravkl.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -76,7 +76,7 @@ const EQUIPMENTS = [
     description: 'สาย HDMI V1.4 3 เมตร UNITEK Y-C139M',
     category: 'Electronics',
     url_pic: 'https://aumento.officemate.co.th/media/catalog/product/4/0/4006668.jpg?imwidth=640',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -84,7 +84,7 @@ const EQUIPMENTS = [
     description: 'TOA Hand Grip Megaphone โทรโข่งแบบมือถือ รุ่น ER-3215',
     category: 'Electronics',
     url_pic: 'http://ustatic.priceza.com/img/productgroup/320592-1-l.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -92,7 +92,7 @@ const EQUIPMENTS = [
     description: 'เซนเซอร์ Ultrasonic Module HC-SR04+ Distance Ultrasonic Sensor',
     category: 'Arduino',
     url_pic: 'https://cdn.sparkfun.com//assets/parts/1/3/5/0/8/15569-Ultrasonic_Distance_Sensor_-_HC-SR04-01a.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -100,7 +100,7 @@ const EQUIPMENTS = [
     description: 'Sound Sensor Module 3 pin Voice Sound Detection Sensor Amplifier LM393 Electret Microphone DIY Kit',
     category: 'Arduino',
     url_pic: 'https://lzd-img-global.slatic.net/g/p/ef10c69885cfb37f30812c16e87e46d6.jpg_720x720q80.jpg_.webp',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -108,7 +108,7 @@ const EQUIPMENTS = [
     description: 'เซนเซอร์ตรวจจับเส้น เดินตามเส้น Infrared barrier module / obstacle avoidance car obstacle avoidance sensor',
     category: 'Arduino',
     url_pic: 'https://inwfile.com/s-dw/38j35e.png',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -116,7 +116,7 @@ const EQUIPMENTS = [
     description: 'Breadboard บอร์ดทดลอง 830 รู MB-102 MB102 Arduino',
     category: 'Arduino',
     url_pic: 'https://lzd-img-global.slatic.net/g/p/mdc/63fd45a95b4623f6cd18928136961d38.jpg_720x720q80.jpg_.webp',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -124,7 +124,7 @@ const EQUIPMENTS = [
     description: 'สาย mophie USB-A พร้อมหัวต่อ Lightning (3 ม.)',
     category: 'Arduino',
     url_pic: 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/HN862?wid=1144&hei=1144&fmt=jpeg&qlt=95&.v=1562973642996',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -132,7 +132,7 @@ const EQUIPMENTS = [
     description: 'หลอด LED 5 mm light emitting diode สี เหลือง ขาว น้ำเงิน เขียว แดง',
     category: 'Arduino',
     url_pic: 'https://cf.shopee.co.th/file/adc812a9fc162bf5ed74a7543bd6f991',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -140,7 +140,7 @@ const EQUIPMENTS = [
     description: 'Jumper wires, F-F, 10 cm',
     category: 'Arduino',
     url_pic: 'https://nettigo.eu/system/images/2580/original.JPG?1510148513',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -148,7 +148,7 @@ const EQUIPMENTS = [
     description: 'Jumper wires, M-M, 10 cm',
     category: 'Arduino',
     url_pic: 'https://nettigo.eu/system/images/2586/original.JPG?1510149243',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -156,7 +156,7 @@ const EQUIPMENTS = [
     description: 'Jumper wires, M-F, 10 cm',
     category: 'Arduino',
     url_pic: 'https://inwfile.com/s-dx/7pgyt8.png',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -164,7 +164,7 @@ const EQUIPMENTS = [
     description: 'กรรไกร 6นิ้ว สก๊อตช์ Multi Purpose 1426',
     category: 'Tools',
     url_pic: 'https://aumento.officemate.co.th/media/catalog/product/O/F/OFM2071800.jpg?imwidth=640',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -172,7 +172,7 @@ const EQUIPMENTS = [
     description: 'เครื่องเจาะกระดาษ ตราช้าง DP-600',
     category: 'Tools',
     url_pic: 'https://www.goodchoiz.com/content/images/thumbs/0042973_%E0%B8%B7%E0%B9%88-%E0%B9%89-dp600-%E0%B8%B5.jpeg',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -180,7 +180,7 @@ const EQUIPMENTS = [
     description: 'Pointer Wireless (รีโมทควบคุมคำสั่งไร้สาย) Presenter HP',
     category: 'Tools',
     url_pic: 'https://www.jib.co.th/img_master/product/original/2018071715132230628_1.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -188,7 +188,7 @@ const EQUIPMENTS = [
     description: 'เครื่องคิดเลข Casio รุ่น FX-991EX สีดำ',
     category: 'Tools',
     url_pic: 'https://d1mqjpb51un8dw.cloudfront.net/catalog/product/5/0/5037589-e1.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -196,7 +196,7 @@ const EQUIPMENTS = [
     description: 'เก้าอี้จัดเลี้ยง ITK TK-66 สีดำ',
     category: 'Furnitures',
     url_pic: 'https://cdn.homepro.co.th/ART_IMAGE/10/423/1042355/447x447/10072021_1042355$Imagec2.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 20,
   },
   {
@@ -204,7 +204,7 @@ const EQUIPMENTS = [
     description: 'โต๊ะพับอเนกประสงค์ รุ่นเซมินาร์ ขนาด 150 X 60 ซม. - สีขาว',
     category: 'Furnitures',
     url_pic: 'https://media.indexlivingmall.com/media/catalog/product/1/2/120021094_op1_Seminar_150cm_WT_2.JPG',
-    status: 'available',
+    status: 'Available',
     amount: 5,
   },
 
@@ -213,7 +213,7 @@ const EQUIPMENTS = [
     description: 'ปลั๊กไฟ(มอก.) 4 ช่อง ยาว 3 เมตร สีขาว โตชิโน ET-914',
     category: 'Electronics',
     url_pic: 'https://aumento.officemate.co.th/media/catalog/product/O/F/OFM8003580.jpg?imwidth=640',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
   {
@@ -221,7 +221,7 @@ const EQUIPMENTS = [
     description: 'กลองสันทนาการ Triplesix',
     category: 'Recreations',
     url_pic: 'https://www.geyemusic.net/wp-content/uploads/2020/02/spd_20190129181753_b2-300x300.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 2,
   },
   {
@@ -229,7 +229,7 @@ const EQUIPMENTS = [
     description: 'ขาตั้งกลองทอมบ้า CMC 601',
     category: 'Recreations',
     url_pic: 'https://www.musicarms.net/wp-content/uploads/2019/01/%E0%B8%82%E0%B8%B2%E0%B8%95%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%81%E0%B8%A5%E0%B8%AD%E0%B8%87%E0%B8%97%E0%B8%AD%E0%B8%A1%E0%B8%9A%E0%B9%89%E0%B8%B2-CMC-601.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 2,
   },
   {
@@ -237,7 +237,7 @@ const EQUIPMENTS = [
     description: 'แทมบูริน Tambourine Hi Hat ไม้แท้ Hi Hat Double Tambourine Drum',
     category: 'Recreations',
     url_pic: 'https://248652-1140738-1-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/03/KD019-020-1.jpg',
-    status: 'available',
+    status: 'Available',
     amount: 10,
   },
 
