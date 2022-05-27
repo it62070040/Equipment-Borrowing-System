a9933f292b059a5166eb9a8cc30de0abf56b4635
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2022-05-27 21:25:52 +0700

    feat:update backend

--- equipment_borrowing_system/back-end/src/app.js
+++ equipment_borrowing_system/back-end/src/app.js
@@ -2,11 +2,11 @@ import { createServer } from 'http'
 
 import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 import { ApolloServer } from 'apollo-server-express'
-import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import express from 'express'
-import { decodeToken, getReqToken } from './lib/generateUserToken'
 
+import { decodeToken, getReqToken } from './lib/generateUserToken'
 import './mongoose-connect'
 // eslint-disable-next-line import/order
 import schema from './graphql'
@@ -16,9 +16,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // app.use(cors({ origin: ['abc.com', '*.test.com'] }))
 app.use(cookieParser())
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }))
+app.use(cors({ origin: ['https://equipment-borrowing-system.vercel.app/'], credentials: true }))
 // app.use(cors({credentials: true }))
-
 
 app.get('/', (req, res) => {
   res.json({ message: 'Server running' })
@@ -50,8 +49,8 @@ const startApolloServer = async () => {
   apolloServer.applyMiddleware({
     app,
     path: '/graphql',
-    cors: {origin: ['http://localhost:3000'], credentials: true },
+    cors: { origin: ['http://localhost:3000'], credentials: true },
   })
-  httpServer.listen({ port: process.env.PORT || 3001})
+  httpServer.listen({ port: process.env.PORT || 3001 })
 }
 startApolloServer()
